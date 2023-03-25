c037445554994fdfc01bfa15a7ff1da354cc466f
George <George>
2023-03-25 13:09:42 +0100

    Challenge 3 "When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example."

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
             title = "Correct"
             score += 1
         } else {
-            title = "Wrong"
+            title = "Wrong, this is the flag of \(countries[sender.tag].uppercased())"
             score -= 1
         }
         
